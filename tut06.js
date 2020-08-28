3575e6311ef2498ebd22b9b499bc14dd5483ddde
Pritam Dhara <pritamdhara914@gmail.com>
2020-08-28 23:04:25 +0530

    version 1.1.6

---
+++ tut06.js
@@ -0,0 +1,27 @@
+console.time('your code took');
+let name = "pritam";
+// let html;
+// html = "<h3> dhara </h3>"+"<p> hdmtp </p>";
+// html = html.concat(' this', ' is');
+
+// console.log(html);
+// console.log(html.length);
+// console.log(html.toUpperCase());
+// console.log(html[24]);
+// console.log(html.indexOf('dhara'), html.indexOf('vjbdvibv'));
+// console.log(html.lastIndexOf(">"), html.charAt(17));
+// console.log(html.endsWith('is'), html.includes('h3>'));
+// console.log(html.substring(4,9), html.slice(4,9), "|---|", html.slice(-18));
+// console.log(html.substring(-5));
+
+// console.log(html.split(" "), html.split(">"));
+// console.log(html.replace('this', 'these'));
+
+let fruit = ['orange', 'apple', 'banana'];
+let myHtml = `Hello ${name} 
+<h1> This is a Heading </h1>
+<p> You like ${fruit[0]} and ${fruit[1]}</p>
+`;
+
+document.body.innerHTML = myHtml;
+console.timeEnd('your code took');
