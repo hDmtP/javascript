d57cb900fb6a1e7a25cf0e0708855b55223fed39
Pritam Dhara <pritamdhara914@gmail.com>
2020-09-07 23:22:13 +0530

    version 1.3.3

---
+++ web dev simplified/es6arrow.js
@@ -0,0 +1,20 @@
+function sum(a,b) {
+    return a+b;
+}
+let sum2=(a,b) => a+b;
+ 
+function isPositive(number) {
+    return number>=0;
+}
+let isPositive2=number => number>=0;
+
+function randomNumber() {
+    return Math.random;
+}
+let randomNumber2=() => Math.random;
+
+
+document.addEventListener('click', function () {
+    console.log('Click');
+})
+document.addEventListener('click', () => console.log('Click'));
